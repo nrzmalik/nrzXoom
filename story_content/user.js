796716bbde1dadf37d1f165960af35c3217be1c4
nrzmalik <125977246+nrzmalik@users.noreply.github.com>
2024-04-11 15:39:13 +0500

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,71 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5e9HyeV5cuA":
+        Script1();
+        break;
+      case "6lW6Mx8wgcm":
+        Script2();
+        break;
+      case "6XZRwiqXBri":
+        Script3();
+        break;
+      case "5ijr5BtDFYi":
+        Script4();
+        break;
+      case "6AzE8PTMz5O":
+        Script5();
+        break;
+      case "5btbECbc4Oj":
+        Script6();
+        break;
+      case "6bXyEaNM9sB":
+        Script7();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  nrzZoom("7");
+}
+
+window.Script2 = function()
+{
+  nrzZoom("5");
+}
+
+window.Script3 = function()
+{
+  nrzZoom("1");
+}
+
+window.Script4 = function()
+{
+  nrzZoom("6");
+}
+
+window.Script5 = function()
+{
+  nrzZoom("3");
+}
+
+window.Script6 = function()
+{
+  nrzZoom("2");
+}
+
+window.Script7 = function()
+{
+  nrzZoom("4");
+}
+
+};
